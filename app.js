58b4d9c448ec8229d0ebcb947d9d23e39fe30c76
Rainbow Senpai <rainbowsoldier200@gmail.com>
2022-10-23 14:33:56 +0300

    s
    s

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting){
+            
             entry.target.classList.add('show');
         } else{
             entry.target.classList.remove('show');
         }
-
     });
 });
 
